92771c2463b4fe25a7f18031a81ddc99a04b3c4e
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-01-27 18:13:31 -0300

    Styles buttons types

--- Paldex/src/views/ListPals.jsx
+++ Paldex/src/views/ListPals.jsx
@@ -7,7 +7,6 @@ import { SVGsearch } from "../assets/SVGsearch";
 export const ListPals = () => {
   const pals = palsData;
   const randomPal = pals[Math.floor(Math.random() * pals.length)];
-  // const [selectedPal, setSelectedPal] = useState(randomPal);
 
   const [selectedPal, setSelectedPal] = useState(null);
 
@@ -18,6 +17,18 @@ export const ListPals = () => {
   const handleClose = () => {
     setSelectedPal(null);
   };
+
+  const types = [
+    "Neutral",
+    "Fire",
+    "Water",
+    "Grass",
+    "Electric",
+    "Ice",
+    "Dark",
+    "Ground",
+    "Dragon",
+  ];
 
   return (
     <>
@@ -30,22 +41,37 @@ export const ListPals = () => {
             </button>
           </div>
           <div className="types">
-            <span className="type">fuego</span>
-            <span className="type">agua</span>
-            <span className="type">tierra</span>
-            <span className="type">aire</span>
-            <span className="type">planta</span>
-            <span className="type">veneno</span>
+            {types.map((type) => (
+              <span className="type" key={type}>
+                <img
+                  src={`./src/assets/images/elements/${type}_icon_mini.webp`}
+                  alt=""
+                />
+              </span>
+            ))}
           </div>
+          {/* <span className="type">
+              <img
+                className="type"
+                src={`./src/assets/images/elements/${types[i]}_icon_mini.webp`}
+                alt=""
+              />
+            </span>
+            <span className="type icon__water">agua</span>
+            <span className="type icon__fire">tierra</span>
+            <span className="type icon__neutral">aire</span>
+            <span className="type icon__grass">planta</span>
+            <span className="type ">veneno</span> */}
+          {/* </div> */}
         </nav>
         <div className="listPals">
           {pals.map((pal) => (
-            <Card key={pal.id} pal={pal}  onClick={() => handleCardClick(pal)}/>
+            <Card key={pal.id} pal={pal} onClick={() => handleCardClick(pal)} />
             // onClick={() => setSelectedPal(pal)}
           ))}
         </div>
       </section>
-      {selectedPal && <ViewCard pal={selectedPal} onClose={handleClose}/>}
+      {selectedPal && <ViewCard pal={selectedPal} onClose={handleClose} />}
     </>
   );
 };
