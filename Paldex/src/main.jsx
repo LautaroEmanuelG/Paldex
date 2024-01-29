54023cfed21611a19c4dc18255e74716122b02af
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-01-29 12:21:37 -0300

    Try change vercel.json

--- Paldex/src/main.jsx
+++ Paldex/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-// import './styles/index.css'
 import "./styles/App.css";
 import "./styles/Card.css";
 import "./styles/ViewCard.css";
