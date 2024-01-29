ad1228179c1d63a552cd920343141e785a6c3cea
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-01-29 11:29:07 -0300

    Add view drops

--- Paldex/src/views/Drops.jsx
+++ Paldex/src/views/Drops.jsx
@@ -1,17 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import dropPals from "../mock/dropPals.json";
 import { Card } from "./components/Card";
+import { SVGupward } from "../assets/SVGupward";
+import { SVGsearch } from "../assets/SVGsearch";
+import { ViewCard } from "./components/ViewCard";
 
 export const Drops = () => {
+  const [selectedDrop, setSelectedDrop] = useState(null);
+  const handleCardClick = (drop) => {
+    setSelectedDrop(drop);
+  };
+
+  const handleClose = () => {
+    setSelectedDrop(null);
+  };
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredDrops = dropPals.filter((drop) =>
+    drop.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const checkScrollPosition = () => {
+      if (window.pageYOffset > window.innerHeight) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", checkScrollPosition);
+
+    return () => {
+      window.removeEventListener("scroll", checkScrollPosition);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <>
+      {showButton && (
+        <button className="returnToTop" onClick={scrollToTop}>
+          <SVGupward />
+        </button>
+      )}
       <section className="list">
+        <nav className="list__nav">
+          <div className="search">
+            <input
+              className="search__input"
+              type="text"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button className="search__btn">
+              <SVGsearch />
+            </button>
+          </div>
+        </nav>
         <article className="listPals">
-          {dropPals.map((dropPal, index) => (
-            <Card key={index} drop={dropPal} />
+          {filteredDrops.map((dropPal, index) => (
+            <Card
+              key={index}
+              drop={dropPal}
+              onClick={() => handleCardClick(dropPal)}
+            />
           ))}
         </article>
       </section>
+      {selectedDrop && <ViewCard drop={selectedDrop} onClose={handleClose} />}
     </>
   );
 };
--- Paldex/src/views/components/ViewCard.jsx
+++ Paldex/src/views/components/ViewCard.jsx
@@ -1,73 +1,102 @@
 import React from "react";
 import { SVGclose } from "../../assets/SVGclose";
+import Pals from "../../mock/pals.json";
+import { Card } from "./Card";
 
-export const ViewCard = ({ pal, onClose }) => {
-  return (
-    <article className="viewCard">
-      <img className="viewCard__img" src={pal.image} alt={pal.name} />
-      <section className="viewCard__box">
-        <h3 className="viewCard__number">N° {pal.key}</h3>
-        <h2 className="viewCard__title">{pal.name}</h2>
-        <div className="viewCard__types">
-          {pal.types.map((type) => (
-            <p key={type} className={`viewCard__type ${type}`}>
-              {type}
-            </p>
-          ))}
+export const ViewCard = ({ pal, onClose, drop }) => {
+  // Traer todos los pals de Pals que coincidan con el nombre de los drops.pals que es un array
+  const droped = drop ? Pals.filter((pal) => drop.pals.includes(pal.name)) : [];
+
+  if (drop) {
+    return (
+      <article className="viewCard">
+        <img className="viewCard__img" src={`/images/drops/${drop.name.replace(/\s/g, "_")}_icon.webp`} alt={drop.name} />
+        <section className="viewCard__box">
+          <h2 className="viewCard__title">{drop.name}</h2>
+          {/* <h4 className="viewCard__entry">Paldex Entry</h4>
+        <span className="viewCard__entry-text">{drop.description}</span> */}
+          <h4 className="viewCard__drops-title">Droped By</h4>
+          <section className="viewCard__dropedBy">
+            {droped.map((dropedBy, index) => (
+              <Card key={index} pal={dropedBy} />
+            ))}
+          </section>
+        </section>
+        <div className={`viewCard__background`}>
+          <button className="viewCard__close" onClick={onClose}>
+            <SVGclose />
+          </button>
         </div>
-        <h4 className="viewCard__entry">Paldex Entry</h4>
-        <span className="viewCard__entry-text">{pal.description}</span>
-        <h4 className="viewCard__suitability-title">Suitability</h4>
-        <section className="viewCard__suitability">
-          {pal.suitability.map((suitability, index) => (
-            <div key={index} className="viewCard__suitability-box">
-              <span className="viewCard__suitability-icon">
-                <img
-                  className="icon"
-                  src={`/images/suitability/${suitability.type.replace(
-                    /\s/g,
-                    "_"
-                  )}_Icon.webp`}
-                  alt=""
-                />
-              </span>
-              <span className="viewCard__suitability-type">
-                {suitability.type}:{" "}
-              </span>
-              <span className="viewCard__suitability-level">
-                {suitability.level}
-              </span>
-            </div>
-          ))}
+      </article>
+    );
+  } else if (pal) {
+    return (
+      <article className="viewCard">
+        <img className="viewCard__img" src={pal.image} alt={pal.name} />
+        <section className="viewCard__box">
+          <h3 className="viewCard__number">N° {pal.key}</h3>
+          <h2 className="viewCard__title">{pal.name}</h2>
+          <div className="viewCard__types">
+            {pal.types.map((type) => (
+              <p key={type} className={`viewCard__type ${type}`}>
+                {type}
+              </p>
+            ))}
+          </div>
+          <h4 className="viewCard__entry">Paldex Entry</h4>
+          <span className="viewCard__entry-text">{pal.description}</span>
+          <h4 className="viewCard__suitability-title">Suitability</h4>
+          <section className="viewCard__suitability">
+            {pal.suitability.map((suitability, index) => (
+              <div key={index} className="viewCard__suitability-box">
+                <span className="viewCard__suitability-icon">
+                  <img
+                    className="icon"
+                    src={`/images/suitability/${suitability.type.replace(
+                      /\s/g,
+                      "_"
+                    )}_Icon.webp`}
+                    alt=""
+                  />
+                </span>
+                <span className="viewCard__suitability-type">
+                  {suitability.type}:{" "}
+                </span>
+                <span className="viewCard__suitability-level">
+                  {suitability.level}
+                </span>
+              </div>
+            ))}
+          </section>
+          <h4 className="viewCard__drops-title">Drops</h4>
+          <section className="viewCard__drops">
+            {pal.drops.map((drop) => (
+              <div key={drop} className="viewCard__drops-text">
+                <span className="viewCard__drops-icon">
+                  <img
+                    className="icon"
+                    src={`/images/drops/${drop.replace(/\s/g, "_")}_icon.webp`}
+                    alt=""
+                  />
+                </span>
+                <span className="viewCard__drops-item">{drop}</span>
+              </div>
+            ))}
+          </section>
+          <h4 className="viewCard__aura-title">Aura</h4>
+          <section className="viewCard__aura">
+            <h5 className="viewCard__aura-name">{pal.aura.name}: </h5>
+            <span className="viewCard__aura-description">
+              {pal.aura.description}
+            </span>
+          </section>
         </section>
-        <h4 className="viewCard__drops-title">Drops</h4>
-        <section className="viewCard__drops">
-          {pal.drops.map((drop) => (
-            <div key={drop} className="viewCard__drops-text">
-              <span className="viewCard__drops-icon">
-                <img
-                  className="icon"
-                  src={`/images/drops/${drop.replace(/\s/g, "_")}_icon.webp`}
-                  alt=""
-                />
-              </span>
-              <span className="viewCard__drops-item">{drop}</span>
-            </div>
-          ))}
-        </section>
-        <h4 className="viewCard__aura-title">Aura</h4>
-        <section className="viewCard__aura">
-          <h5 className="viewCard__aura-name">{pal.aura.name}: </h5>
-          <span className="viewCard__aura-description">
-            {pal.aura.description}
-          </span>
-        </section>
-      </section>
-      <div className={`viewCard__background ${pal.types[0]}`}>
-        <button className="viewCard__close" onClick={onClose}>
-          <SVGclose />
-        </button>
-      </div>
-    </article>
-  );
+        <div className={`viewCard__background ${pal.types[0]}`}>
+          <button className="viewCard__close" onClick={onClose}>
+            <SVGclose />
+          </button>
+        </div>
+      </article>
+    );
+  }
 };
